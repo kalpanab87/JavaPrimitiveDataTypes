b64cf86d4ba30c942ba1b0c48c481f4f01c65625
Kalpanab87 <kalpana7.ece@gmail.com>
2019-12-14 22:57:21 +0000

    Default values for Java primitive datatypes

---
+++ src/learnProgramming/MyPrimitiveTest.java
@@ -0,0 +1,31 @@
+package learnProgramming;
+
+public class MyPrimitiveTest {
+
+    static byte byteVariable;
+    static short shortVariable;
+    static int intVariable;
+    static long longVariable;
+
+    static double doubleVariable;
+    static float floatVariable;
+
+    static char charVariable;
+    static boolean booleanVariable;
+
+    public static void main(String[] args) {
+
+        System.out.println("default value of byte is" +byteVariable); //0
+        System.out.println("default value of short is"+shortVariable); //0
+        System.out.println("default value of int is"+intVariable); //0
+        System.out.println("default value of long is"+longVariable); //0
+        System.out.println("default value of double is"+doubleVariable); //0.0
+        System.out.println("default value of float is"+floatVariable); //0.0
+        System.out.println("default value of char is"+charVariable); //(blank)
+        System.out.println("default value of boolean is"+booleanVariable); //false
+
+    }
+
+
+
+}
